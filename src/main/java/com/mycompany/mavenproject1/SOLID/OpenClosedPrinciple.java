ff97b34244bf28fc3a334979d567da53f758cc15
Sankalp Adhya <101995802+SanDevil23@users.noreply.github.com>
2025-01-24 21:52:23 +0530

    hashing with SHA

---
+++ src/main/java/com/mycompany/mavenproject1/SOLID/OpenClosedPrinciple.java
@@ -0,0 +1,11 @@
+package com.mycompany.mavenproject1.SOLID;
+
+
+
+
+public class OpenClosedPrinciple {
+
+    // according to the OCP
+    // a class should be open for extension
+    // but closed for modification
+}
